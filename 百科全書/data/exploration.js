39ccc1afbe1430ce3e68793e568bda464029df3c
Hsing-Tzu <daisyko1234@gmail.com>
2023-03-25 15:41:24 +0800

    update

---
+++ 百科全書/data/exploration.js
@@ -0,0 +1,94 @@
+const word = [
+    {
+        href:"",
+        src:"./image/cryptocurrency.jpeg",
+        ctitle:"加密貨幣",
+        etitle:"Cryptocurrency",
+    },
+    {
+        href:"",
+        src:"./image/blockchain.jpg",
+        ctitle:"區塊鏈",
+        etitle:"Blockchain",
+    },
+    {
+        href:"",
+        src:"./image/AR.png",
+        ctitle:"擴增實境",
+        etitle:"Augmented Reality",
+    },
+    {
+        href:"",
+        src:"./image/VR.jpg",
+        ctitle:"虛擬實境",
+        etitle:"Virtual Reality",
+    },{
+        href:"",
+        src:"./image/metaverse.jpg",
+        ctitle:"元宇宙",
+        etitle:"Metaverse",
+    },
+    {
+        href:"",
+        src:"./image/AI.jpg",
+        ctitle:"人工智慧",
+        etitle:"Artificial Intelligence",
+    },{
+        href:"",
+        src:"./image/cybersecurity.jpg",
+        ctitle:"網路安全",
+        etitle:"Cybersecurity",
+    },
+    {
+        href:"",
+        src:"./image/IOT.jpg",
+        ctitle:"物聯網",
+        etitle:"Internet of Things",
+    },{
+        href:"",
+        src:"./image/datascience.jpg",
+        ctitle:"資料科學",
+        etitle:"Data Science",
+    },
+    {
+        href:"",
+        src:"./image/bigdata.jpg",
+        ctitle:"大數據",
+        etitle:"Big Data",
+    },{
+        href:"",
+        src:"./image/smartcontract.jpg",
+        ctitle:"智能合約",
+        etitle:"Smart Contract",
+    },
+    {
+        href:"",
+        src:"./image/machinelearning.jpg",
+        ctitle:"機器學習",
+        etitle:"Machine Learning",
+    },
+    {
+        href:"",
+        src:"./image/hybridcloud.jpg",
+        ctitle:"混合雲",
+        etitle:"Hybrid Cloud",
+    },
+    {
+        href:"",
+        src:"./image/fungibletoken.jpg",
+        ctitle:"同質化代幣",
+        etitle:"Fungible Token",
+    },
+    {
+        href:"",
+        src:"./image/nonfungibletoken.jpg",
+        ctitle:"非同質化代幣",
+        etitle:"Non-Fungible Token",
+    },{
+        href:"",
+        src:"./image/semifungibletoken.jpg",
+        ctitle:"半同質化代幣",
+        etitle:"Semi-Fungiable Token",
+    },
+
+]
---
+++ 百科全書/script/renderexploration.js
@@ -0,0 +1,17 @@
+const wordsimageElements = [...document.getElementsByClassName('words-image')];
+wordsimageElements.forEach((wordimageElement, idx) => {
+  const aElement = document.createElement('a');
+  const imgElement = document.createElement('img');
+  aElement.href = word[idx].href;
+  imgElement.src = word[idx].src;
+  wordimageElement.appendChild(aElement);
+  aElement.appendChild(imgElement);
+});
+
+const wordstextElements = [...document.getElementsByClassName('words-text')];
+wordstextElements.forEach((wordtextElement, idx) => {
+  const aElement = document.createElement('a');
+  aElement.href = word[idx].href;
+  aElement.innerText = word[idx].ctitle + " \n " + word[idx].etitle;
+  wordtextElement.appendChild(aElement);
+});
